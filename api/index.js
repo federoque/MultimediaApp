d01fd5ed723b37b44cc2f0187750f0e9b447e9a5
federoque <roquefede@gmail.com>
2022-06-07 19:10:18 -0300

    Finalizado

--- api/index.js
+++ api/index.js
@@ -4,18 +4,18 @@ const {fillCategories} = require('./src/controllers/categories.controller')
 const { signUpAdmin } = require('./src/controllers/auth.contoller')
 const { bulkCreate } = require('./src/controllers/content.controller')
 
-sequelize.sync({force: false})
+sequelize.sync({force: true})
 .then(()=> console.log("Connected to DB"))
 .then(()=>{
-//     fillCategories()
-//     signUpAdmin()
-//     console.log('Categories and Admin saved on DB')
-// })
-// .then(()=>{
+    fillCategories()
+    signUpAdmin()
+    console.log('Categories and Admin saved on DB')
+})
+.then(()=>{
     app.listen(3001)
     console.log('Server listen on port', 3001)
 })
-// .then(()=>setTimeout(() => {
-//     bulkCreate()
-// }, 1000))
+.then(()=>setTimeout(() => {
+    bulkCreate()
+}, 1000))
 .catch((error)=> console.log(error))
